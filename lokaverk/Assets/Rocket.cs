1c3b487bb49d318c37c3bf66ddbb261fff466a7f
elmarolafss <elmarol250@nemi.tskoli.is>
2018-11-26 18:26:49 +0000

    demo

---
+++ lokaverk/Assets/Rocket.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip levelUp;
+    [SerializeField] AudioClip die;
+    Rigidbody rigidbody;
+    AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+    
+
+    // Use this for initialization
+    void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+   
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive) {return;}
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                audioSource.PlayOneShot(levelUp);
+                state = State.Transcending;
+                Invoke("LoadNext",1f);
+                break;
+            case "Landing":
+                break;
+            default:
+                state = State.Dying;
+                audioSource.PlayOneShot(die);
+                Invoke("LoadCurrent",1f);
+                break;
+        }
+    }
+
+    private void LoadCurrent()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void LoadNext()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(mainEngine);
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+    private void RespondToRotateInput()
+    {
+        rigidbody.freezeRotation = true;
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        rigidbody.freezeRotation = false;
+    }
+}
